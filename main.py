ec2eb500770efce5e3245e8808b97ebb06b877a3
wlodarskajulia <wlodarskajulia@DESKTOP-EPEIILF.localdomain>
2026-05-18 13:59:13 +0200

    main.py adjusted

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def main():
-    print("Hello from aml-project!")
+from datasets import load_dataset
+from src.preprocessing.preprocessing import preprocess
+
+
+def main(): 
+    # data = load_dataset("TeddyVDobreva/AML_project_dataset", split='train')
+    # data = preprocess(data)
+    # data.push_to_hub("TeddyVDobreva/AML_project_preprocessed_dataset", split='train')
+    data = load_dataset("TeddyVDobreva/AML_project_preprocessed_dataset", split='train')
 
 
 if __name__ == "__main__":
