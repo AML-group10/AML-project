7e847b65a43f0bf4c859140a1d05c7b63427d969
Teodora Veselinova Dobreva T.V.Dobreva@student.rug.nl <s5888441@node4.internal.hpc.rug.nl>
2026-05-18 21:12:57 +0200

    model no working pipeline 1

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import torch
+from diffusers.pipelines.wuerstchen.pipeline_wuerstchen_prior import  WuerstchenPriorPipeline
+from diffusers.pipelines.wuerstchen import DEFAULT_STAGE_C_TIMESTEPS
+import matplotlib.pyplot as plt
+
+
 def main():
-    print("Hello from aml-project!")
+    pipeline = WuerstchenPriorPipeline.from_pretrained("wuerstchen-prior-naruto-model/prior_pipeline", torch_dtype=torch.float16).to("cuda")
+
+    # caption = "A cute bird naruto holding a shield"
+    # images = pipeline(
+    #     caption,
+    #     width=1024,
+    #     height=1536,
+    #     prior_timesteps=DEFAULT_STAGE_C_TIMESTEPS,
+    #     prior_guidance_scale=4.0,
+    #     num_images_per_prompt=2,
+    # ).images
+    # plt.imshow(images[0])
+    # plt.show()
 
 
 if __name__ == "__main__":
