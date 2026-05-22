c8b453742c451a215b9378f529a2929ec9d13091
Teodora Veselinova Dobreva T.V.Dobreva@student.rug.nl <s5888441@omni1.internal.hpc.rug.nl>
2026-05-22 09:47:41 +0200

    data upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import os
+import subprocess
 
 import numpy as np
 from datasets import Dataset, DatasetDict, load_dataset
@@ -33,7 +34,14 @@ def split_data(dataset: Dataset) -> Dataset:
     return train_test_valid_dataset
 
 
-def prepare_dataset(dataset: DatasetDict, local_dir):
+def prepare_dataset(dataset: DatasetDict, local_dir: str) -> None:
+    """
+    Downloads and prepares the dataset for training
+
+    Args:
+        dataset (Dataset): the dataset to be prepared
+        local_dir (str): a directory to save the dataset
+    """
     os.makedirs(local_dir, exist_ok=True)
     with open(f"{local_dir}metadata.jsonl", "w") as outfile:
         for index in range(len(dataset)):
@@ -46,17 +54,62 @@ def prepare_dataset(dataset: DatasetDict, local_dir):
             json.dump(entry, outfile)
             outfile.write("\n")
 
+def save_dataset_to_hub(path: str) -> None:
+    """
+    Saves a local dataset to the hub
+
+    Args:
+        path (str): local path to dataset
+    """
+    train_dataset = load_dataset("imagefolder", data_dir=(path + "train/"))
+    valid_dataset = load_dataset("imagefolder", data_dir=(path + "valid/"))
+    test_dataset = load_dataset("imagefolder", data_dir=(path + "test/"))
+    train_dataset.push_to_hub("AML-group10/AML_project_preprocessed_dataset", "train")
+    valid_dataset.push_to_hub("AML-group10/AML_project_preprocessed_dataset", "valid")
+    test_dataset.push_to_hub("AML-group10/AML_project_preprocessed_dataset", "test")
+
+def resize_images(path):
+    pass
+
+def run_training(data_dir: str) -> None:
+    """
+    Runs a preprocessing and training loop
+
+    Args:
+        data_dir (str): a directory containing the dataset to train on
+    """
+    train_cmd = [
+        "accelerate", "launch", "src/models/training/lora_training.py",
+        "--pretrained_model_name_or_path", "segmind/tiny-sd",
+        "--mixed_precision", "fp16",
+        "--train_data_dir", "./faces/train",
+        "--output_dir", "./lora-output",
+        "--use_peft",
+        "--lora_r", "4",
+        "--lora_alpha", "4", 
+        "--resolution", "768",
+        "--train_batch_size", "8",
+        "--gradient_accumulation_steps", "100",
+        "--num_train_epochs", "1",
+        "--learning_rate", "1e-4",
+        "--caption_column", "prompt",
+        "--push_to_hub",
+        "--seed", "67",
+        "--allow_tf32",
+    ]
+
+    subprocess.run(train_cmd, check=True)
+
 
 def main():
-    # data = load_dataset("TeddyVDobreva/AML_project_dataset", split='train')
+    # data = load_dataset("AML-group10/AML_project_dataset", split='train')
     # data = preprocess(data)
-    # data.push_to_hub("TeddyVDobreva/AML_project_preprocessed_dataset", split='train')
-    data = load_dataset("TeddyVDobreva/AML_project_preprocessed_dataset", split="train")
-    data = split_data(data)
-    prepare_dataset(data["train"], "./faces/train/")
-    prepare_dataset(data["valid"], "./faces/valid/")
-    prepare_dataset(data["test"], "./faces/test/")
-    print(data)
+    # data = split_data(data)
+    # prepare_dataset(data["train"], "./faces/train/")
+    # prepare_dataset(data["valid"], "./faces/valid/")
+    # prepare_dataset(data["test"], "./faces/test/")
+    save_dataset_to_hub("./faces/")
+    # run_training("AML-group10/AML_project_preprocessed_dataset")
 
 
 if __name__ == "__main__":
