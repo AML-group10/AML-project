2ad065c8aeb649ab63c27c5c08a3ee6c5ef6e8cc
Teodora Veselinova Dobreva T.V.Dobreva@student.rug.nl <s5888441@node55.internal.hpc.rug.nl>
2026-05-24 11:23:34 +0200

    add epochs to hp  tuning

--- src/models/training/hyperparameter_tuning.sh
+++ src/models/training/hyperparameter_tuning.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
 learning_rates=(1e-4 3e-4 5e-4)
+num_epochs=(10 15 20)
 
 for lr in "${learning_rates[@]}"; do
-    echo "Learning rate $lr"
+    for epochs in "${num_epochs[@]}"; do
+        echo "Learning rate $lr, epochs $epochs"
 
-    python3 src/models/training/lora_training.py \
-    --pretrained_model_name_or_path="segmind/tiny-sd" \
-    --dataset_name="AML-group10/AML_project_preprocessed_dataset" \
-    --dataset_config_name="train" \
-    --output_dir="./AML-group10/${lr}hyperparameter_tuning" \
-    --use_peft \
-    --lora_r=4 \
-    --lora_alpha=4 \
-    --resolution=256 \
-    --train_batch_size=512 \
-    --gradient_accumulation_steps=1 \
-    --num_train_epochs=10 \
-    --learning_rate=$lr\
-    --validation_prompt="a man with curly black hair, blue eyes and a moustache" \
-    --seed=67
+        python3 src/models/training/lora_training.py \
+            --pretrained_model_name_or_path="segmind/tiny-sd" \
+            --dataset_name="AML-group10/AML_project_preprocessed_dataset" \
+            --dataset_config_name="train" \
+            --output_dir="./AML-group10/${lr}_${epochs}_hyperparameter_tuning" \
+            --use_peft \
+            --lora_r=4 \
+            --lora_alpha=4 \
+            --resolution=256 \
+            --train_batch_size=512 \
+            --gradient_accumulation_steps=1 \
+            --num_train_epochs=$epochs \
+            --learning_rate=$lr\
+            --validation_prompt="a man with curly black hair, blue eyes and a moustache" \
+            --seed=67
 
 done
