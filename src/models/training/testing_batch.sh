be5cb4261429737b60ac9030987de3284324eee7
Sara Sophia Lopotaru S.Lopotaru@student.rug.nl <s5916771@omni19.internal.hpc.rug.nl>
2026-05-24 10:58:29 +0200

    changed hyperparameters

--- src/models/training/testing_batch.sh
+++ src/models/training/testing_batch.sh
@@ -4,6 +4,7 @@ python3 src/models/training/lora_training.py \
     --pretrained_model_name_or_path="segmind/tiny-sd" \
     --dataset_name="AML-group10/AML_project_preprocessed_dataset" \
     --dataset_config_name="train" \
+    --max_train_samples=200 \
     --output_dir="./AML-group10/testing_batch_size" \
     --use_peft \
     --lora_r=4 \
@@ -11,7 +12,7 @@ python3 src/models/training/lora_training.py \
     --resolution=512 \
     --train_batch_size=32 \
     --gradient_accumulation_steps=1 \
-    --num_train_epochs=100 \
+    --num_train_epochs=10 \
     --learning_rate=1e-4 \
     --validation_prompt="people with toothbrushes" \
     --seed=67
