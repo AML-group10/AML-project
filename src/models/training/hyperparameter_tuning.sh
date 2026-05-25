01512821dfd35693dd10d82aa2498458be4f09dc
Teodora Veselinova Dobreva T.V.Dobreva@student.rug.nl <s5888441@omni35.internal.hpc.rug.nl>
2026-05-25 11:21:04 +0200

    models with 5e-4 lr

--- src/models/training/hyperparameter_tuning.sh
+++ src/models/training/hyperparameter_tuning.sh
@@ -8,19 +8,22 @@ for lr in "${learning_rates[@]}"; do
         echo "Learning rate $lr, epochs $epochs"
 
         python3 src/models/training/lora_training.py \
-            --pretrained_model_name_or_path="segmind/tiny-sd" \
-            --dataset_name="AML-group10/AML_project_preprocessed_dataset" \
-            --dataset_config_name="train" \
-            --output_dir="./AML-group10/${lr}_${epochs}_hyperparameter_tuning" \
-            --use_peft \
-            --lora_r=4 \
-            --lora_alpha=4 \
-            --resolution=256 \
-            --train_batch_size=512 \
-            --gradient_accumulation_steps=1 \
-            --num_train_epochs=$epochs \
-            --learning_rate=$lr\
-            --validation_prompt="a man with curly black hair, blue eyes and a moustache" \
-            --seed=67
-
+        --pretrained_model_name_or_path="segmind/tiny-sd" \
+        --dataset_name="AML-group10/AML_project_preprocessed_dataset" \
+        --dataset_config_name="train" \
+        --output_dir="./AML-group10/${lr}_${epochs}_hyperparameter_tuning" \
+        --use_peft \
+        --lora_r=4 \
+        --lora_alpha=4 \
+        --resolution=256 \
+        --train_batch_size=512 \
+        --gradient_accumulation_steps=1 \
+        --num_train_epochs=$epochs \
+        --learning_rate=$lr \
+        --caption_column="prompt" \
+        --push_to_hub \
+        --allow_tf32 \
+        --validation_epochs \
+        --validation_prompt="a man with curly black hair, blue eyes and a moustache" \
+        --seed=67
 done
--- src/models/training/tuning_hyperparameters.sh
+++ src/models/training/tuning_hyperparameters.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
+lr=5e-4
 
 python3 src/models/training/lora_training.py \
     --pretrained_model_name_or_path="segmind/tiny-sd" \
     --dataset_name="AML-group10/AML_project_preprocessed_dataset" \
     --dataset_config_name="train" \
-    --output_dir="./AML-group10/1e-4_20_hyperparameter_tuning" \
+    --output_dir="./AML-group10/${lr}_30hyperparameter_tuning" \
     --use_peft \
     --lora_r=4 \
     --lora_alpha=4 \
     --resolution=256 \
     --train_batch_size=512 \
     --gradient_accumulation_steps=1 \
-    --num_train_epochs=20 \
-    --learning_rate=1e-4 \
+    --num_train_epochs=30 \
+    --learning_rate=$lr \
+    --caption_column="prompt" \
+    --push_to_hub \
+    --validation_epochs=1 \
+    --allow_tf32 \
     --validation_prompt="a man with curly black hair, blue eyes and a moustache" \
     --seed=67
