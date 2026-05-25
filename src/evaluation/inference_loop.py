647466a9e0bc953b9e2d92ad3446a37f0d32008e
Julia Zofia Wlodarska J.Z.Wlodarska@student.rug.nl <s5965780@node27.internal.hpc.rug.nl>
2026-05-25 11:37:28 +0200

    inference_loop finished

--- src/evaluation/inference_loop.py
+++ src/evaluation/inference_loop.py
@@ -1,10 +1,11 @@
 from datasets import load_dataset
 import os
 from evaluation_functions import run_evaluation
+from models.training.inference import load_and_set_lora_ckpt
 
 # Load validation prompts from HuggingFace
-dataset = load_dataset("AML-group10/AML_project_preprocessed_dataset", "valid")
-prompts = [item["prompt"] for item in dataset["train"]]
+dataset = load_dataset("AML-group10/AML_project_preprocessed_dataset", split="valid")
+prompts = [item["prompt"] for item in dataset]
 
 # Attributes for evaluation
 attributes = {
@@ -19,26 +20,31 @@ attributes = {
 
 # Loop over all 9 models
 models = [
-    "AML-group10/1e-4_20hyperparameter_tuning",
-    "AML-group10/1e-4_15hyperparameter_tuning",
-    "AML-group10/1e-4_10hyperparameter_tuning",
-    "AML-group10/5e-4_10hyperparameter_tuning",
-    "AML-group10/5e-4_15hyperparameter_tuning",
-    "AML-group10/5e-4_20hyperparameter_tuning",
-    "AML-group10/3e-4_10hyperparameter_tuning",
-    "AML-group10/3e-4_15hyperparameter_tuning",
-    "AML-group10/3e-4_20hyperparameter_tuning",
+    ("AML-group10/1e-4_20hyperparameter_tuning", 200),
+    ("AML-group10/1e-4_15hyperparameter_tuning", 150),
+    ("AML-group10/1e-4_10hyperparameter_tuning", 100),
+    ("AML-group10/5e-4_10hyperparameter_tuning", 100),
+    ("AML-group10/5e-4_15hyperparameter_tuning", 150),
+    ("AML-group10/5e-4_20hyperparameter_tuning", 200),
+    ("AML-group10/3e-4_10hyperparameter_tuning", 100),
+    ("AML-group10/3e-4_15hyperparameter_tuning", 150),
+    ("AML-group10/3e-4_20hyperparameter_tuning", 200)
 ]
 
-for model_name in models:
-    # Create output folder for this model
-    folder_name = model_name.split("/")[-1] 
+os.makedirs("validation_results", exist_ok=True)
+
+for model_name, step_count in models:
+    folder_name = model_name.split("/")[-1]
     os.makedirs(f"generated/{folder_name}", exist_ok=True)
+
+    base = DiffusionPipeline.from_pretrained("segmind/tiny-sd", torch_dtype=torch.float32)
+    model = load_and_set_lora_ckpt(base, step_count)  
+    print("Model loaded", model_name)
     
-    # Loop over all validation prompts
     for i, prompt in enumerate(prompts):
-        pass
-        # GENERATE IMAGE AND SAVE IT IN THE FOLDER
+        generator = torch.Generator(device="cpu").manual_seed(67)
+        image = model(prompt, num_inference_steps=30, generator=generator).images[0]
+        image.save(f"generated/{folder_name}/image_{i}.jpeg")
 
 # Run evaluation on each folder
 for model_name in models:
