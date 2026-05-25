6bc7a82acbee7d22833ee562f1155b0a84741b5f
Sara Sophia Lopotaru S.Lopotaru@student.rug.nl <s5916771@node73.internal.hpc.rug.nl>
2026-05-25 13:23:28 +0200

    created backend, hope it works

---
+++ deployment/original_server.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from diffusers import DiffusionPipeline
+from pydantic import BaseModel
+from fastapi.responses import Response
+import io
+import torch
+
+model = DiffusionPipeline.from_pretrained("segmind/tiny-sd", torch_dtype=torch.bfloat16)
+model.load_lora_weights("AML-group10/5e-4_30hyperparameter_tuning", weight_name="300_lora.pt")
+
+  
+class ModelInput(BaseModel):
+    prompt: str
+
+app = FastAPI()
+
+@app.post("/")
+async def generate_image(input_text: ModelInput):
+    generated_image = model(input_text.prompt).images[0]
+    buf = io.BytesIO()
+    generated_image.save(buf, format="PNG")
+    buf.seek(0)
+    return Response(content=buf.read(), media_type="image/png")
