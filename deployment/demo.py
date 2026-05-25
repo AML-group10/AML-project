b56067ee118db0cf30014f0953b5e23602270506
Sophia Lopotaru <sophialopotaru@gmail.com>
2026-05-25 16:11:57 +0200

    work in progress

--- deployment/demo.py
+++ deployment/demo.py
@@ -1,4 +1,12 @@
-import streamlit as str
+import requests
+import streamlit as st
 
-st.title = "Text to Image (good for meme generation...)"
-prompt = st.input_text("What's your prompt?")
+st.title("Text to Image (good for meme generation...)")
+prompt = st.text_input("What's your prompt?")
+generate_button = st.button("Generate image", disabled=not prompt)
+
+if generate_button:
+    try:
+        response = requests.post("http://localhost:8000/", json={"prompt": prompt})
+    except:
+        pass
--- deployment/original_server.py
+++ deployment/original_server.py
@@ -1,20 +1,26 @@
-from fastapi import FastAPI
-from diffusers import DiffusionPipeline
-from pydantic import BaseModel
-from fastapi.responses import Response
 import io
+
 import torch
+from fastapi import FastAPI
+from fastapi.responses import Response
+from pydantic import BaseModel
+
+from diffusers import DiffusionPipeline
 
 model = DiffusionPipeline.from_pretrained("segmind/tiny-sd", torch_dtype=torch.bfloat16)
-model.load_lora_weights("AML-group10/5e-4_30hyperparameter_tuning", weight_name="300_lora.pt")
+model.load_lora_weights(
+    "AML-group10/5e-4_30hyperparameter_tuning", weight_name="300_lora.pt"
+)
 
-  
+
 class ModelInput(BaseModel):
     prompt: str
 
-app = FastAPI()
 
-@app.post("/")
+app = FastAPI(redirect_slashes=False)
+
+
+@app.post("/generate")
 async def generate_image(input_text: ModelInput):
     generated_image = model(input_text.prompt).images[0]
     buf = io.BytesIO()
