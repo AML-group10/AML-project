2757146ccf5c7b03549ba55069b02fcebbc6a725
Sophia Lopotaru <sophialopotaru@gmail.com>
2026-05-29 11:33:03 +0200

    connected backend to frontend, need to add errors, otherwise deployment is done

--- deployment/demo.py
+++ deployment/demo.py
@@ -1,5 +1,8 @@
+import io
+
 import requests
 import streamlit as st
+from PIL import Image
 
 st.title("Text to Image (good for meme generation...)")
 prompt = st.text_input("What's your prompt?")
@@ -7,6 +10,13 @@ generate_button = st.button("Generate image", disabled=not prompt)
 
 if generate_button:
     try:
-        response = requests.post("http://localhost:8000/", json={"prompt": prompt})
+        response = requests.post(
+            "http://localhost:8000/generate", json={"prompt": prompt}
+        )
+        print(response)
+        print(response.json)
+        response.raise_for_status()
+        generated_image = Image.open(io.BytesIO(response.content))
+        st.image(generated_image)
     except:
         pass
--- src/evaluation/inference_loop.py
+++ src/evaluation/inference_loop.py
@@ -1,16 +1,54 @@
-from datasets import load_dataset
+import json
 import os
-from evaluation_functions import run_evaluation
-from diffusers import DiffusionPipeline 
-import torch
-
 import sys
+
+import torch
+from datasets import load_dataset
+from diffusers import DiffusionPipeline
+from evaluation_functions import run_evaluation
+from huggingface_hub import hf_hub_download
+from peft import LoraConfig, LoraModel, set_peft_model_state_dict
+
 sys.path.append("/scratch/s5965780/AML-project/src")
-from models.training.inference import load_and_set_lora_ckpt
+
+
+def load_and_set_lora_ckpt(pipe, repo_id, step_count, device="cpu"):
+    config_path = hf_hub_download(
+        repo_id=repo_id, filename=f"{step_count}_lora_config.json"
+    )
+    weights_path = hf_hub_download(repo_id=repo_id, filename=f"{step_count}_lora.pt")
+
+    with open(config_path, "r") as f:
+        lora_config = json.load(f)
+
+    lora_checkpoint_sd = torch.load(weights_path)
+    unet_lora_ds = {
+        k: v for k, v in lora_checkpoint_sd.items() if "text_encoder_" not in k
+    }
+    text_encoder_lora_ds = {
+        k.replace("text_encoder_", ""): v
+        for k, v in lora_checkpoint_sd.items()
+        if "text_encoder_" in k
+    }
+
+    unet_config = LoraConfig(**lora_config["peft_config"])
+    pipe.unet = LoraModel(pipe.unet, unet_config, "default")
+    set_peft_model_state_dict(pipe.unet, unet_lora_ds)
+
+    if "text_encoder_peft_config" in lora_config:
+        text_encoder_config = LoraConfig(**lora_config["text_encoder_peft_config"])
+        pipe.text_encoder = LoraModel(pipe.text_encoder, text_encoder_config, "default")
+        set_peft_model_state_dict(pipe.text_encoder, text_encoder_lora_ds)
+
+    pipe.to(device)
+    return pipe
+
 
 # Load validation prompts from HuggingFace
 device = "cpu"
-dataset = load_dataset("AML-group10/AML_project_preprocessed_dataset", "valid", split="train")
+dataset = load_dataset(
+    "AML-group10/AML_project_preprocessed_dataset", "valid", split="train"
+)
 dataset = dataset.shuffle(seed=42).select(range(700))
 prompts = [item["prompt"][0] for item in dataset]
 
@@ -36,9 +74,7 @@ attributes = {
     ("AML-group10/3e-4_20_hyperparameter_tuning", 200) sophie """
 
 # Loop over all 9 models
-models = [
-    ("AML-group10/1e-4_20_hyperparameter_tuning", 200)
-]
+models = [("AML-group10/3e-4_10_hyperparameter_tuning", 100)]
 
 os.makedirs("real_validation", exist_ok=True)
 os.makedirs("validation_results", exist_ok=True)
@@ -50,11 +86,13 @@ for model_name, step_count in models:
     folder_name = model_name.split("/")[-1]
     os.makedirs(f"generated/{folder_name}", exist_ok=True)
 
-    base = DiffusionPipeline.from_pretrained("segmind/tiny-sd", torch_dtype=torch.float32).to(device)
+    base = DiffusionPipeline.from_pretrained(
+        "segmind/tiny-sd", torch_dtype=torch.float32
+    ).to(device)
     model = load_and_set_lora_ckpt(base, model_name, step_count, device)
     generator = torch.Generator(device=device).manual_seed(67)
     print("Model loaded", folder_name)
-    
+
     for i, prompt in enumerate(prompts):
         image = model(prompt, num_inference_steps=30, generator=generator).images[0]
         image.save(f"generated/{folder_name}/image_{i}.jpeg")
@@ -67,6 +105,6 @@ for model_name, _ in models:
         real_images_path="real_validation/",
         captions=prompts,
         attributes_dict=attributes,
-        output_file=f"validation_results/{folder_name}_results.json", 
-        compute_bias=False
+        output_file=f"validation_results/{folder_name}_results.json",
+        compute_bias=False,
     )
