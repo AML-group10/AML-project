3c8c9b3436e79c31b517486d325a0bad73b1693b
Sara Sophia Lopotaru S.Lopotaru@student.rug.nl <s5916771@node64.internal.hpc.rug.nl>
2026-05-31 16:16:17 +0200

    generated image with 3e4 10 epochs model

--- src/evaluation/inference_loop.py
+++ src/evaluation/inference_loop.py
@@ -1,17 +1,53 @@
-from datasets import load_dataset
+import json
 import os
-from evaluation_functions import run_evaluation
-from diffusers import DiffusionPipeline 
-import torch
-
 import sys
-sys.path.append("/scratch/s5965780/AML-project/src")
-from models.training.inference import load_and_set_lora_ckpt
+
+import torch
+from datasets import load_dataset
+from diffusers import DiffusionPipeline
+from evaluation_functions import run_evaluation
+from huggingface_hub import hf_hub_download
+from peft import LoraConfig, LoraModel, set_peft_model_state_dict
+
+sys.path.append("/scratch/s5916771/AML-project/src")
+
+
+def load_and_set_lora_ckpt(pipe, repo_id, step_count, device="cpu"):
+    config_path = hf_hub_download(
+        repo_id=repo_id, filename=f"{step_count}_lora_config.json"
+    )
+    weights_path = hf_hub_download(repo_id=repo_id, filename=f"{step_count}_lora.pt")
+
+    with open(config_path, "r") as f:
+        lora_config = json.load(f)
+
+    lora_checkpoint_sd = torch.load(weights_path)
+    unet_lora_ds = {
+        k: v for k, v in lora_checkpoint_sd.items() if "text_encoder_" not in k
+    }
+    text_encoder_lora_ds = {
+        k.replace("text_encoder_", ""): v
+        for k, v in lora_checkpoint_sd.items()
+        if "text_encoder_" in k
+    }
+
+    unet_config = LoraConfig(**lora_config["peft_config"])
+    pipe.unet = LoraModel(pipe.unet, unet_config, "default")
+    set_peft_model_state_dict(pipe.unet, unet_lora_ds)
+
+    if "text_encoder_peft_config" in lora_config:
+        text_encoder_config = LoraConfig(**lora_config["text_encoder_peft_config"])
+        pipe.text_encoder = LoraModel(pipe.text_encoder, text_encoder_config, "default")
+        set_peft_model_state_dict(pipe.text_encoder, text_encoder_lora_ds)
+
+    pipe.to(device)
+    return pipe
 
 # Load validation prompts from HuggingFace
-device = "cpu"
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 dataset = load_dataset("AML-group10/AML_project_preprocessed_dataset", "valid", split="train")
-dataset = dataset.shuffle(seed=42).select(range(700))
+# range was 700
+dataset = dataset.shuffle(seed=42).select(range(462, 700))
 prompts = [item["prompt"][0] for item in dataset]
 
 # Attributes for evaluation
@@ -37,14 +73,16 @@ attributes = {
 
 # Loop over all 9 models
 models = [
-    ("AML-group10/1e-4_20_hyperparameter_tuning", 200)
+    ("AML-group10/3e-4_10_hyperparameter_tuning", 100)
 ]
 
 os.makedirs("real_validation", exist_ok=True)
 os.makedirs("validation_results", exist_ok=True)
 
+'''
 for i, item in enumerate(dataset):
     item["image"].save(f"real_validation/image_{i}.jpeg")
+'''
 
 for model_name, step_count in models:
     folder_name = model_name.split("/")[-1]
@@ -55,10 +93,10 @@ for model_name, step_count in models:
     generator = torch.Generator(device=device).manual_seed(67)
     print("Model loaded", folder_name)
     
-    for i, prompt in enumerate(prompts):
+    for i, prompt in enumerate(prompts, start = 462):
         image = model(prompt, num_inference_steps=30, generator=generator).images[0]
         image.save(f"generated/{folder_name}/image_{i}.jpeg")
-
+'''
 # Run evaluation on each folder
 for model_name, _ in models:
     folder_name = model_name.split("/")[-1]
@@ -70,3 +108,4 @@ for model_name, _ in models:
         output_file=f"validation_results/{folder_name}_results.json", 
         compute_bias=False
     )
+'''
