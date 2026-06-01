f5215db399504af0300df705756a6b83f6bc77e5
Sophia Lopotaru <sophialopotaru@gmail.com>
2026-06-01 20:47:11 +0200

    added errors to deployment. deployment should be done now

--- deployment/demo.py
+++ deployment/demo.py
@@ -4,19 +4,24 @@ import requests
 import streamlit as st
 from PIL import Image
 
-st.title("Text to Image (good for meme generation...)")
+st.title("Text to Image Generation")
 prompt = st.text_input("What's your prompt?")
 generate_button = st.button("Generate image", disabled=not prompt)
 
 if generate_button:
-    try:
-        response = requests.post(
-            "http://localhost:8000/generate", json={"prompt": prompt}
-        )
-        print(response)
-        print(response.json)
-        response.raise_for_status()
-        generated_image = Image.open(io.BytesIO(response.content))
-        st.image(generated_image)
-    except:
-        pass
+    with st.spinner("Wait while the image is being generated"):
+        try:
+            response = requests.post(
+                "http://localhost:8000/generate", json={"prompt": prompt}
+            )
+            if response.status_code == 200:
+                response.raise_for_status()
+                generated_image = Image.open(io.BytesIO(response.content))
+                st.image(generated_image)
+                st.balloons()
+            elif response.status_code == 404:
+                st.error("Error 404: Page not found")
+            elif response.status_code == 500:
+                st.error("Error 500: Internal server error.")
+        except requests.exceptions.ConnectionError:
+            st.error("Connection to API failed.")
--- deployment/original_server.py
+++ deployment/original_server.py
@@ -2,14 +2,14 @@ import io
 
 import torch
 from diffusers import DiffusionPipeline
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import Response
 from pydantic import BaseModel
 
 from src.models.training.inference import load_and_set_lora_ckpt
 
 base = DiffusionPipeline.from_pretrained("segmind/tiny-sd", torch_dtype=torch.float32)
-model = load_and_set_lora_ckpt(base, "AML-group10/5e-4_30hyperparameter_tuning", 300)
+model = load_and_set_lora_ckpt(base, "AML-group10/5e-4_20hyperparameter_tuning", 200)
 
 
 class ModelInput(BaseModel):
@@ -21,15 +21,19 @@ app = FastAPI(redirect_slashes=False)
 
 @app.get("/health")
 async def health():
-    return Response(content="yo mama", media_type="txt")
+    return Response(content="This works.", media_type="txt")
 
 
 @app.post("/generate")
 async def generate_image(input_text: ModelInput):
-    print(input_text)
-    print(input_text.prompt)
-    generated_image = model(input_text.prompt, num_inference_steps=30).images[0]
-    buf = io.BytesIO()
-    generated_image.save(buf, format="PNG")
-    buf.seek(0)
-    return Response(content=buf.read(), media_type="image/png")
+    if not input_text.prompt:
+        raise HTTPException(status_code=404, detail="Prompt not found.")
+
+    try:
+        generated_image = model(input_text.prompt, num_inference_steps=30).images[0]
+        buf = io.BytesIO()
+        generated_image.save(buf, format="PNG")
+        buf.seek(0)
+        return Response(content=buf.read(), media_type="image/png", status_code=200)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Text to image genration failed.")
